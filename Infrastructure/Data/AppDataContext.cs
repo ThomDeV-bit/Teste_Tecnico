7d7ef56ec37d2cc38037dd9ea1e04d8824dd978b
ThomDeV-bit <120723852+ThomDeV-bit@users.noreply.github.com>
2025-06-08 23:22:03 -0400

    feat: App Data Context

---
+++ Infrastructure/Data/AppDataContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using Teste_Tecnico.Domain.Entities;
+
+namespace Teste_Tecnico.Infrastructure.Data
+{
+    public class AppDataContext : DbContext
+    {
+        public AppDataContext(DbContextOptions<AppDataContext> options)
+        : base(options)
+        {
+
+        }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
