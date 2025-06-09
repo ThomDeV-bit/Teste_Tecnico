211b455027e12f5bac78adfbd856f959527070cc
ThomDeV-bit <120723852+ThomDeV-bit@users.noreply.github.com>
2025-06-08 23:21:11 -0400

    feat: Utilitarios

---
+++ Aplication/Communs/Utilitarios.cs
@@ -0,0 +1,16 @@
+﻿namespace Teste_Tecnico.Aplication.Communs
+{
+    public class Utilitarios
+    {
+        private static int saltGenerate = 10;
+
+        public static string HashSenhaUsuario(string senha)
+        {
+            string salt = BCrypt.Net.BCrypt.GenerateSalt(saltGenerate);
+
+            string senhaHash = BCrypt.Net.BCrypt.HashPassword(senha, salt);
+
+            return senhaHash;
+        }
+    }
+}
